a06f9ef01749135a1e5c0002d1cb5bb28a075268
preethibuddy25 <preethiyogasundaram25@gmail.com>
2022-03-19 09:25:31 +0530

    first commit

---
+++ log.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Site Visits Report</title>
+  </head>
+
+  <body>
+
+      <h1>Site Visits Report</h1>
+
+      <table border = '1'>
+        <tr>
+          <th>No.</th>
+          <th>Visitor</th>
+          <th>Total Visits</th>
+        </tr>
+
+        <?php
+
+            try {
+
+                $redis = new Redis();
+                $redis->connect('127.0.0.1', 6379);
+                $redis->auth('EXAMPLE_PASSWORD');
+
+                $siteVisitsMap = 'siteStats';                          
+
+                $siteStats = $redis->HGETALL($siteVisitsMap);
+
+                $i = 1; 
+
+                foreach ($siteStats as $visitor => $totalVisits) {
+
+                    echo "<tr>";
+                      echo "<td align = 'left'>"   . $i . "."     . "</td>";
+                      echo "<td align = 'left'>"   . $visitor     . "</td>";
+                      echo "<td align = 'right'>"  . $totalVisits . "</td>";
+                    echo "</tr>";
+                    
+                    $i++;
+                }
+
+            } catch (Exception $e) {
+                echo $e->getMessage();
+            }
+
+        ?>
+
+      </table>
+  </body>
+
+</html>
